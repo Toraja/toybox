bfb6b19de09c750a05c583819b5c04f63d2c2c5d
Toraja <toraja.blend.french@gmail.com>
2019-08-05 08:25:58 +0900

    Added fish functions

---
+++ fish/functions/wrap_in_echo.fish
@@ -0,0 +1,11 @@
+function wrap_in_echo_double --description 'echo -n "$commandline" | <copycmd>'
+	set -l cmd (commandline)
+	commandline -r ''
+	commandline -i "echo -n \"$cmd\" | $copycmd"
+end
+
+function wrap_in_echo_single --description 'echo -n \'$commandline\' | <copycmd>'
+	set -l cmd (commandline)
+	commandline -r ''
+	commandline -i "echo -n '$cmd' | $copycmd"
+end
