3b3efd5f824366ba5c2b3626572c00e4c273e348
Toraja <toraja.blend.french@gmail.com>
2019-08-06 08:57:19 +0900

    Misc update on fish config

--- fish/config.fish
+++ fish/config.fish
@@ -6,17 +6,15 @@ set -x VISUAL vim
 set -x GOPATH $HOME/go
 
 # PATH
-# add_unique -p PATH $GOPATH
 not contains $GOPATH $PATH; and set -x PATH $GOPATH $PATH
 
 # fish variables
-set fish_dir ~/.config/fish
-set fish_conf $fish_dir/config.fish
-set fish_myfuncs $fish_dir/myfuncs
-# add_unique -p fish_function_path $fish_myfuncs
+set fish_conf $__fish_config_dir/config.fish
+set fish_myfuncs $__fish_config_dir/myfuncs
 set fish_function_path $fish_myfuncs $fish_function_path
 set fish_prompt_pwd_dir_length 0 # disable shortening path in prompt_pwd
 
+# alias
 alias rm='rm -i'
 alias cp='cp -ip'
 alias mv='mv -i'
