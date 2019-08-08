78a8c8d71fac0316a8b1f775548226241df30025
Toraja <toraja.blend.french@gmail.com>
2019-08-09 08:48:20 +0900

    Added fish abbreviation

--- fish/config.fish
+++ fish/config.fish
@@ -27,3 +27,27 @@ if test (which nvim)
 	alias view='nvim -R'
 	alias vimdiff='nvim -d'
 end
+
+# abbr
+## git
+abbr --add --global gb git branch
+abbr --add --global gs git status
+abbr --add --global gl git log
+abbr --add --global gd git git diff
+abbr --add --global gdc git diff --cached
+abbr --add --global ga git add
+abbr --add --global gal git add .
+abbr --add --global gcm git commit -m
+abbr --add --global gco git checkout
+abbr --add --global gst git stash
+abbr --add --global gsp git stash pop
+abbr --add --global gstm git stash push -m
+## docker
+abbr --add --global dc docker-compose
+abbr --add --global dcx docker-compose exec
+abbr --add --global dcd docker-compose down -v
+abbr --add --global dcu docker-compose up -d\; docker-compose logs --follow
+abbr --add --global dcr docker-composer restart
+## go
+abbr --add --global got go test
+abbr --add --global gotr go test -run
