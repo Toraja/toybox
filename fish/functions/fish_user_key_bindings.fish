c4f02621babf51b2ace8ef57e14619d0b38e582a
Toraja <toraja.blend.french@gmail.com>
2019-08-10 23:08:47 +0900

    Updated keybinding

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -9,22 +9,20 @@ function fish_user_key_bindings
 
     # edit
     bind \eh backward-kill-word
+    bind \e\cw backward-kill-bigword
+    bind \e\cd kill-bigword
+    bind \e\cu kill-whole-line
 
     # search
     bind \ep history-token-search-backward
     bind \en history-token-search-forward
-
-    # selection
-    bind \cv begin-selection
-    bind \cx\cx swap-selection-start-stop repaint
-    bind \cx\x20 end-selection repaint
-    bind \ek kill-selection end-selection repaint
 
     # misc
     bind \e\? __fish_man_page
     bind \e\ci complete-and-search
     bind \e\\ __fish_paginate
 
+    # wrapper
     if set -q TMUX
         set -g copycmd 'tmux load-buffer -'
     else if test -f /mnt/c/Windows/System32/clip.exe
@@ -52,11 +50,19 @@ function fish_user_key_bindings
     bind \e\'p "wrap_process \' \'"
     bind \e\'j "wrap_job \' \'"
 
-    # Leave this as reference about different mode
-    # Need to bind <BS>, <DEL> and arrow keys etc as self-insert for those just input weird chars
-    # bind -M selection '' self-insert
-    # bind -m selection \cv begin-selection
-    # bind -M selection -m default \eo swap-selection-start-stop repaint
-    # bind -M selection -m default \ex end-selection repaint
-    # bind -M selection -m default \ek kill-selection end-selection repaint
+    # selection
+    bind -M selection \cf forward-char
+    bind -M selection \cb backward-char
+    bind -M selection \ef forward-word
+    bind -M selection \eb backward-word
+    bind -M selection \e\cf forward-bigword 
+    bind -M selection \e\cb backward-bigword
+    bind -M selection \ca beginning-of-line
+    bind -M selection \ce end-of-line
+    bind -m selection \cx\x20 begin-selection
+    bind -M selection -m default \cx\cx swap-selection-start-stop repaint
+    bind -M selection -m default \cx\x20 end-selection repaint
+    bind -M selection -m default \e end-selection repaint
+    bind -M selection -m default \cw kill-selection end-selection repaint
+    bind -M selection -m default \ek kill-selection end-selection repaint
 end
