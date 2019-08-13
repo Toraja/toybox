cf5a09f8e6b935281d189ff2cda2b0036b658d28
Toraja <toraja.blend.french@gmail.com>
2019-08-13 22:13:55 +0900

    Added fish keybining

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -6,12 +6,15 @@ function fish_user_key_bindings
     bind \e\cb backward-bigword
     bind \x1D forward-jump
     bind \e\x1D backward-jump
+    bind \e\; repeat-jump
+    bind \e, repeat-jump-reverse
 
     # edit
     bind \eh backward-kill-word
     bind \e\cw backward-kill-bigword
     bind \e\cd kill-bigword
     bind \e\cu kill-whole-line
+    bind \el downcase-word
 
     # search
     bind \ep history-token-search-backward
@@ -20,7 +23,8 @@ function fish_user_key_bindings
     # misc
     bind \e\? __fish_man_page
     bind \e\ci complete-and-search
-    bind \e\\ __fish_paginate
+    bind \cx\cp __fish_paginate
+    bind \cx\cl __fish_list_current_token
 
     # wrapper
     if set -q TMUX
@@ -51,18 +55,24 @@ function fish_user_key_bindings
     bind \e\'j "wrap_job \' \'"
 
     # selection
-    bind -M selection \cf forward-char
-    bind -M selection \cb backward-char
-    bind -M selection \ef forward-word
-    bind -M selection \eb backward-word
-    bind -M selection \e\cf forward-bigword 
-    bind -M selection \e\cb backward-bigword
-    bind -M selection \ca beginning-of-line
-    bind -M selection \ce end-of-line
     bind -m selection \cx\x20 begin-selection
     bind -M selection -m default \cx\cx swap-selection-start-stop repaint
     bind -M selection -m default \cx\x20 end-selection repaint
     bind -M selection -m default \e end-selection repaint
     bind -M selection -m default \cw kill-selection end-selection repaint
     bind -M selection -m default \ek kill-selection end-selection repaint
+    bind -M selection \cp up-line
+    bind -M selection \cn down-line
+    bind -M selection \cf forward-char
+    bind -M selection \cb backward-char
+    bind -M selection \ef forward-word
+    bind -M selection \eb backward-word
+    bind -M selection \e\cf forward-bigword
+    bind -M selection \e\cb backward-bigword
+    bind -M selection \ca beginning-of-line
+    bind -M selection \ce end-of-line
+    bind -M selection \x1D forward-jump
+    bind -M selection \e\x1D backward-jump
+    bind -M selection \e\; repeat-jump
+    bind -M selection \e, repeat-jump-reverse
 end
