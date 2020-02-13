cb6203168689581e29b16dbab208c2deea9fef9c
Toraja <toraja.blend.french@gmail.com>
2020-02-13 19:05:28 +0900

    fish: fixed add_unique()

--- fish/functions/add_unique.fish
+++ fish/functions/add_unique.fish
@@ -2,16 +2,17 @@ function add_unique --description "Add values to list if not contained, appendin
     argparse --name=add_unique --min-args=2 -x p,a 'p/prepend' 'a/append' -- $argv
     or return 1
 
+    test -n "$_flag_prepend"
+    and set flag '--prepend'
+    or set flag '--append'
+
     set listname $argv[1]
-    set list (eval echo '$'$listname)
     set values $argv[2..-1]
 
     for v in $values
-        not contains $v $list
-        and set -a new_values $v
+        not contains $v (string split ' ' (eval echo '$'$listname))
+        and set -g $flag $listname $v
     end
 
-    test -n "$_flag_prepend"
-    and set -gp $listname $new_values
-    or set -ga $listname $new_values
+    return 0
 end
