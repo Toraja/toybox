141f33eefee331a27183ddeeb97e83f62b8117d6
Toraja <toraja.blend.french@gmail.com>
2021-06-18 21:26:02 +0900

    add java ide (though language server is not working)

--- vim/helper/java-lsp.sh
+++ vim/helper/java-lsp.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-lsp_home="$HOME/garage/tools/jdt-language-server"
-JAR="$lsp_home/lsp/plugins/org.eclipse.equinox.launcher_*.jar"
+# Reference: https://github.com/autozimu/LanguageClient-neovim/wiki/Java
+
+# TODO set value to repo's root directory
+server={{ your server installation location }}
+
 java \
-	-Declipse.application=org.eclipse.jdt.ls.core.id1 \
-	-Dosgi.bundles.defaultStartLevel=4 \
-	-Declipse.product=org.eclipse.jdt.ls.core.product \
-	-Dlog.level=ALL \
-	-Xmx1G \
-	-jar $(echo "$JAR") \
-	-configuration "$lsp_home/lsp/config_linux/" \
-	-data "$lsp_home/workspace/" \
-	--add-modules=ALL-SYSTEM \
-	--add-opens java.base/java.util=ALL-UNNAMED \
-	--add-opens java.base/java.lang=ALL-UNNAMED \
+    -Declipse.application=org.eclipse.jdt.ls.core.id1 \
+    -Dosgi.bundles.defaultStartLevel=4 \
+    -Declipse.product=org.eclipse.jdt.ls.core.product \
+    -noverify \
+    -Xms1G \
+    -jar $server/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/plugins/org.eclipse.equinox.launcher_1.*.jar \
+    -configuration $server/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/config_linux/ \
+    "$@"
+
+# below options not necessary? (old version used these)
+# -Dlog.level=ALL \
+# --add-modules=ALL-SYSTEM \
+# --add-opens java.base/java.util=ALL-UNNAMED \
+# --add-opens java.base/java.lang=ALL-UNNAMED \
