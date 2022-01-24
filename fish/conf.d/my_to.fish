8fb2466d76e373ce0f26f49d7f2e7f60b678fef7
Toraja <toraja.blend.french@gmail.com>
2022-01-24 13:20:42 +0900

    fish: change TO_DIR location conditionally

--- fish/conf.d/my_to.fish
+++ fish/conf.d/my_to.fish
@@ -1,2 +1,9 @@
 # Overwrite default bookmark location of to-fish
-set -U TO_DIR ~/.local/share/fish/tofish
+if test -d ~/workspace
+    if  not test -d ~/workspace/config/fish/
+        mkdir -p ~/workspace/config/fish/
+    end
+    set -U TO_DIR ~/workspace/config/fish/tofish
+else
+    set -U TO_DIR ~/.local/share/fish/tofish
+end
