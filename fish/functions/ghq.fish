1945a94b09316eb7d4e46486f1ad142795c5b7aa
Toraja <toraja.blend.french@gmail.com>
2022-03-28 16:00:50 +0900

    fish: ghq dirty also lists repos with unpushed commits

--- fish/functions/ghq.fish
+++ fish/functions/ghq.fish
@@ -8,11 +8,17 @@ function ghq --description "ghq wrapper"
 
 end
 
+# With status option, output of `git status` is appended.
 function ghq_dirty --description "List dirty repos paths"
     argparse 's/status' -- $argv
 
     for repo in (command ghq list --full-path)
-        git -C $repo diff-index --quiet HEAD -- ; or begin
+        git -C $repo diff-index --quiet HEAD --
+        if [ $status -eq 0 ]
+            test -z (git -C $repo remote); and continue
+            set --local current_branch (git -C $repo branch | grep '*' | awk '{ print $2 }')
+            git -C $repo diff-index --quiet origin/$current_branch -- ; or echo "$repo [unpushed commits]"
+        else
             echo $repo
             test -n "$_flag_status"; and git -C $repo status --short
         end
