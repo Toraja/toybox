f04711d667247536d912eeba3a3c2bdc0b1c8460
Toraja <Toraja@users.noreply.github.com>
2022-08-10 13:33:31 +0900

    vim: enhance formatting fish file

--- vim/rtp/ftplugin/fish.lua
+++ vim/rtp/ftplugin/fish.lua
@@ -4,8 +4,13 @@ vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 
 vim.cmd([[
+  function s:FishFormat()
+    let [_, l:currentline, l:currentcol, _] = getpos('.')
+    silent %!fish_indent
+    call cursor(l:currentline, l:currentcol)
+  endfunction
   augroup fish_format
     autocmd!
-    autocmd BufWritePre *.fish silent %!fish_indent
+    autocmd BufWritePre *.fish call s:FishFormat()
   augroup END
 ]])
