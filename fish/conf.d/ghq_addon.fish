0bbe7f84f5db4cf03b2e03902c90686a5ae13c17
Toraja <Toraja@users.noreply.github.com>
2022-08-10 23:18:01 +0900

    fish: add completion for ghq dirty

---
+++ fish/conf.d/ghq_addon.fish
@@ -0,0 +1,2 @@
+complete -f -c ghq -n __fish_ghq_needs_command -a dirty -d "List path of dirty repositories"
+complete -f -c ghq -n "__fish_ghq_using_command dirty" -l status -s s -d "Display the status of each repository"
