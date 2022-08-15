bf72cf778890f3da87daf64c9f19bba5d11a45bf
Toraja <Toraja@users.noreply.github.com>
2022-08-15 14:59:22 +0900

    vim: ceate autocmd using nvim_create_autocmd() instead of vimscript

--- vim/rtp/ftplugin/fish.lua
+++ vim/rtp/ftplugin/fish.lua
@@ -3,14 +3,18 @@ vim.bo.expandtab = true
 vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 
-vim.cmd([[
-  function s:FishFormat()
-    let [_, l:currentline, l:currentcol, _] = getpos('.')
-    silent %!fish_indent
-    call cursor(l:currentline, l:currentcol)
-  endfunction
-  augroup fish_format
-    autocmd!
-    autocmd BufWritePre *.fish call s:FishFormat()
-  augroup END
-]])
+local function fish_format()
+  local cursor_position = vim.fn.getpos('.')
+  local current_line, current_column = cursor_position[2], cursor_position[3]
+  vim.cmd('silent %!fish_indent')
+  vim.fn.cursor(current_line, current_column)
+end
+
+local fish_format_augroud_id = vim.api.nvim_create_augroup('fish_format', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = fish_format_augroud_id,
+  pattern = '*.fish',
+  callback = function()
+    fish_format()
+  end,
+})
--- vim/rtp/ftplugin/lua.lua
+++ vim/rtp/ftplugin/lua.lua
@@ -1,6 +1,8 @@
-vim.cmd([[
-  augroup lua_format
-    autocmd!
-    autocmd BufWritePre *.lua silent lua vim.lsp.buf.formatting_sync()
-  augroup END
-]])
+local lua_format_augroud_id = vim.api.nvim_create_augroup('lua_format', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = lua_format_augroud_id,
+  pattern = '*.lua',
+  callback = function()
+    vim.lsp.buf.formatting_sync()
+  end,
+})
