76e1fea085e09ef5171f671876fd5812c5938eb4
Toraja <Toraja@users.noreply.github.com>
2022-09-01 17:04:52 +0900

    vim: replace tagbar with symbols-outline

--- vim/rtp/lua/plugins.lua
+++ vim/rtp/lua/plugins.lua
@@ -258,9 +258,53 @@ return require('packer').startup(function(use)
   }
   use {
     'preservim/tagbar',
+    disable = true,
     config = function()
       vim.keymap.set('n', '<Leader>o', '<Cmd>TagbarToggle<CR>')
       vim.keymap.set('n', '<Leader>O', '<Cmd>TagbarOpen fj<CR>')
+    end
+  }
+  use {
+    'simrat39/symbols-outline.nvim',
+    config = function()
+      local symbols_outline = require("symbols-outline")
+      symbols_outline.setup()
+
+      local function is_window_open_in_current_tab(win_id)
+        return vim.fn.win_id2win(win_id) ~= 0
+      end
+
+      local function is_outline_open_in_current_tab()
+        if not symbols_outline.view:is_open() then
+          return false
+        end
+        return is_window_open_in_current_tab(symbols_outline.view.winnr)
+      end
+
+      local function symbols_outline_focus()
+        if is_outline_open_in_current_tab() then
+          vim.fn.win_gotoid(symbols_outline.view.winnr)
+          return
+        end
+
+        if symbols_outline.view:is_open() then
+          symbols_outline.close_outline()
+        end
+
+        symbols_outline.open_outline()
+      end
+
+      local function symbols_outline_toggle()
+        if symbols_outline.view:is_open() then
+          symbols_outline.close_outline()
+          return
+        end
+
+        symbols_outline.open_outline()
+      end
+
+      vim.keymap.set('n', '<Leader>o', symbols_outline_toggle)
+      vim.keymap.set('n', '<Leader>O', symbols_outline_focus)
     end
   }
   use {
