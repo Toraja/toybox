b4adf9e64efc18cb20f45823166ca45d7a692dfd
Toraja <Toraja@users.noreply.github.com>
2022-09-22 23:40:27 +0900

    taskwarrior: rename awaiting tag to zzz

--- taskwarrior/shortcut/awaiting.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-task $@ _unique tags | sed 's/,/\n/g' | grep -q --line-regexp awaiting
-
-if [[ $? -eq 0 ]]; then
-  task $@ modify -awaiting 2>/dev/null
-else
-  task $@ modify +awaiting 2>/dev/null
-fi
