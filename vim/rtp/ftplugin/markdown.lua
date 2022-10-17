e14fe0b07957b01a546c9eeaf6689522a828eb22
Toraja <Toraja@users.noreply.github.com>
2022-10-17 16:12:58 +0900

    vim: limit keymap to buffer

--- vim/rtp/ftplugin/markdown.lua
+++ vim/rtp/ftplugin/markdown.lua
@@ -3,26 +3,27 @@ local started_symbol = 'STARTED '
 local blocked_symbol = 'BLOCKED '
 
 local mkdnflow = require('mkdnflow')
-vim.keymap.set('n', ']]', function() mkdnflow.cursor.toHeading(nil) end, { desc = 'Jump to next heading' })
-vim.keymap.set('n', '[[', function() mkdnflow.cursor.toHeading(nil, {}) end, { desc = 'Jump to previous heading' })
+vim.keymap.set('n', ']]', function() mkdnflow.cursor.toHeading(nil) end, { desc = 'Jump to next heading', buffer = true })
+vim.keymap.set('n', '[[', function() mkdnflow.cursor.toHeading(nil, {}) end,
+  { desc = 'Jump to previous heading', buffer = true })
 vim.keymap.set('n', 'o', function() mkdnflow.lists.newListItem(false, false, true, 'i', 'o') end,
-  { desc = 'Add list item below' })
+  { desc = 'Add list item below', buffer = true })
 vim.keymap.set('n', 'O', function() mkdnflow.lists.newListItem(false, true, true, 'i', 'O') end,
-  { desc = 'Add list item above' })
-vim.keymap.set('n', '<C-]>', function() mkdnflow.links.followLink() end, { desc = 'Follow link' })
+  { desc = 'Add list item above', buffer = true })
+vim.keymap.set('n', '<C-]>', function() mkdnflow.links.followLink() end, { desc = 'Follow link', buffer = true })
 vim.keymap.set('n', '<C-t><C-]>', function() vim.cmd('tab split'); mkdnflow.links.followLink() end,
-  { desc = 'Follow link in tab' })
+  { desc = 'Follow link in tab', buffer = true })
 vim.keymap.set('n', '<C-w><C-]>', function() vim.cmd('split'); mkdnflow.links.followLink() end,
-  { desc = 'Follow link in horizontal window' })
+  { desc = 'Follow link in horizontal window', buffer = true })
 
 require('keymap.which-key-helper').register_with_editable('Markdown', '-', '-', {
-  { 'b', 'ToDoToggleStatus ' .. blocked_symbol, { desc = 'Toggle BLOCKED' } },
-  { 'c', 'ConcealToggle', { desc = 'Toggle conceallevel between 0 and 2' } },
-  { 'd', 'MkdnToggleToDo', { desc = 'Toggle TODO status' } },
-  -- { 'D', '', { desc = 'Remove TODO Checkbox' } },
-  { 'f', 'MkdnFoldSection', { desc = 'Fold Section' } },
-  { 'F', 'MkdnUnfoldSection', { desc = 'Unfold Section' } },
-  -- { 'l', '', { desc = 'Toggle list' } },
-  { 'p', 'ToDoAddPomodoro', { desc = 'Add pomodoro' } },
-  { 's', 'ToDoToggleStatus ' .. started_symbol, { desc = 'Toggle STARTED' } },
+  { 'b', 'ToDoToggleStatus ' .. blocked_symbol, { desc = 'Toggle BLOCKED', buffer = true } },
+  { 'c', 'ConcealToggle', { desc = 'Toggle conceallevel between 0 and 2', buffer = true } },
+  { 'd', 'MkdnToggleToDo', { desc = 'Toggle TODO status', buffer = true } },
+  -- { 'D', '', { desc = 'Remove TODO Checkbox', buffer = true } },
+  { 'f', 'MkdnFoldSection', { desc = 'Fold Section', buffer = true } },
+  { 'F', 'MkdnUnfoldSection', { desc = 'Unfold Section', buffer = true } },
+  -- { 'l', '', { desc = 'Toggle list', buffer = true } },
+  { 'p', 'ToDoAddPomodoro', { desc = 'Add pomodoro', buffer = true } },
+  { 's', 'ToDoToggleStatus ' .. started_symbol, { desc = 'Toggle STARTED', buffer = true } },
 })
