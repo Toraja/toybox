8d8304cac06bceea67999db3d6b05fdefbfdd239
Toraja <Toraja@users.noreply.github.com>
2022-10-18 10:52:52 +0900

    vim: add helpful comment

--- vim/rtp/lua/keymap/which-key-helper.lua
+++ vim/rtp/lua/keymap/which-key-helper.lua
@@ -11,6 +11,7 @@ end
 function M.register_with_editable(prompt, prefix_key, edit_key, cmds)
   local edit_prefix_key = prefix_key .. edit_key
   local wk = require("which-key")
+  -- Use which-key only to set the name of grouping
   wk.register({ [prefix_key] = { name = prompt } })
   wk.register({ [edit_prefix_key] = { name = "Edit command" } })
 
