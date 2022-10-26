63e706f075f8c6d2f4884d2f71c94783272e60e4
Toraja <Toraja@users.noreply.github.com>
2022-10-26 20:34:18 +0900

    vim: replace deprecated functionality

--- vim/rtp/lua/plugins.lua
+++ vim/rtp/lua/plugins.lua
@@ -821,7 +821,7 @@ return require('packer').startup(function(use)
 
       local lspconfig = require('lspconfig')
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
       capabilities.textDocument.completion.completionItem.resolveSupport = {
         properties = {
           'documentation',
