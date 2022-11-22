15fe2a687f5a878631fb7f6f9b300353134f850d
Toraja <Toraja@users.noreply.github.com>
2022-11-22 21:03:52 +0900

    vim: add command to toggle todo cancelled

--- vim/rtp/after/syntax/markdown.vim
+++ vim/rtp/after/syntax/markdown.vim
@@ -1,3 +1,4 @@
 syntax keyword Blocked BLOCKED
 syntax keyword Todo STARTED
-syntax match Identifier " *- \[.\]"
+syntax match Identifier ".*- \[[^~]\]"
+syntax match Folded ".*- \[\~\].*"
--- vim/rtp/ftplugin/markdown.lua
+++ vim/rtp/ftplugin/markdown.lua
@@ -1,6 +1,34 @@
 -- local todo_line_ptn = '^\([ \t]*- \[.\+\]\) \(.*\)'
 local started_symbol = 'STARTED '
 local blocked_symbol = 'BLOCKED '
+local todo_line_ptn = '^([ \t]*)(- %[(.)%])( .*)'
+
+local function is_todo_line(line)
+  return string.match(line, todo_line_ptn) ~= nil
+end
+
+local function is_todo_line(line)
+  return string.match(line, todo_line_ptn) ~= nil
+end
+
+local function get_todo_checkbox_state(line)
+  if not is_todo_line(line) then
+    return
+  end
+
+  return string.gsub(line, todo_line_ptn, '%3')
+end
+
+local function todo_toggle_cancelled(line)
+  if not is_todo_line(line) then
+    return
+  end
+
+  local new_checkbox_state = get_todo_checkbox_state(line) == '~' and ' ' or '~'
+  local cancelled_todo = string.gsub(line, todo_line_ptn, '%1- [' .. new_checkbox_state .. ']%4')
+  vim.fn.setline('.', cancelled_todo)
+end
+vim.api.nvim_create_user_command('ToggleToDoCancelled', function() todo_toggle_cancelled(vim.fn.getline('.')) end, {})
 
 local mkdnflow = require('mkdnflow')
 vim.keymap.set('n', ']]', function() mkdnflow.cursor.toHeading(nil) end, { desc = 'Jump to next heading', buffer = true })
@@ -26,4 +54,7 @@ require('keymap.which-key-helper').register_with_editable('Markdown', '-', '-', 
   -- { 'l', '', { desc = 'Toggle list', buffer = true } },
   { 'p', 'ToDoAddPomodoro', { desc = 'Add pomodoro', buffer = true } },
   { 's', 'ToDoToggleStatus ' .. started_symbol, { desc = 'Toggle STARTED', buffer = true } },
+  { 'x', 'ToggleToDoCancelled', { desc = 'Cancel TODO', buffer = true } },
 }, { buffer = 0 })
+
+
