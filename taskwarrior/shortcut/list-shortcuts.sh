5633e70b8e355f7b45130d6feaaca3adf164c714
Toraja <Toraja@users.noreply.github.com>
2023-02-17 13:08:32 +0900

    taskwarrior: add commands to list custom shortcuts

---
+++ taskwarrior/shortcut/list-shortcuts.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eo pipefail
+
+shortcuts=$(task show shortcut | sed -e 's/uda\.taskwarrior-tui\.//')
+
+# "" is required to preserve newlines
+num_commands="$(echo "$shortcuts" | grep shortcuts | sed -e 's/shortcuts\.//')"
+declare -A num_command_map
+while read line; do
+  arr=($line)
+  num=${arr[0]} 
+  cmd=$(basename ${arr[1]})
+  rest=${arr[2]} 
+  num_command_map+=([$num]="$cmd $rest")
+done <<< "$num_commands"
+
+num_keys=$(echo "$shortcuts" | grep keyconfig | sed -e 's/keyconfig\.shortcut//')
+declare -A num_key_map
+while read line; do
+  arr=($line)
+  num=${arr[0]} 
+  key=${arr[1]} 
+  num_key_map+=([$num]="$key")
+done <<< "$num_keys"
+
+for num in "${!num_command_map[@]}"
+do
+  key="${num_key_map[$num]}"
+  printf "  %s   %1s   %s\n" "$num" "$key" "${num_command_map[$num]}"
+done | sort | less
