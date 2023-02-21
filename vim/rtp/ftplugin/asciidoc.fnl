301836bedc78f86838e142d4aed25464a5ba8fa8
Toraja <Toraja@users.noreply.github.com>
2023-02-21 09:21:57 +0900

    vim: ditch fennel

--- vim/rtp/ftplugin/asciidoc.fnl
+++
@@ -1,1 +0,0 @@
-(vim.keymap.set ["n" "i"] "<M-;>" "<Cmd>call edit#base#ToggleTrailing(' +')<CR>" { :desc "Toggle new line" :buffer true })
---
+++ vim/rtp/ftplugin/helm.lua
@@ -0,0 +1,32 @@
+local surround_config = require('nvim-surround.config')
+require('nvim-surround').buffer_setup({
+  surrounds = {
+    v = {
+      ---@diagnostic disable-next-line: assign-type-mismatch
+      add = { "{{ ", " }}" },
+      find = function()
+        return surround_config.get_selection({ pattern = '{{ ?[^}]* ?}}' })
+      end,
+      ---@diagnostic disable-next-line: assign-type-mismatch
+      delete = '^(.. ?)().-( ?..)()$',
+      change = {
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        target = '({{ ?)()[^}]*( }})()',
+      },
+    },
+    -- The below works but not useful
+    -- V = {
+    --   ---@diagnostic disable-next-line: assign-type-mismatch
+    --   add = { "{{ $", " }}" },
+    --   find = function()
+    --     return surround_config.get_selection({ pattern = '{{ ?%$[^}]* ?}}' })
+    --   end,
+    --   ---@diagnostic disable-next-line: assign-type-mismatch
+    --   delete = '^(....)().-( ?..)()$',
+    --   change = {
+    --     ---@diagnostic disable-next-line: assign-type-mismatch
+    --     target = '({{ %$)()[^}]*( }})()',
+    --   },
+    -- },
+  },
+})
