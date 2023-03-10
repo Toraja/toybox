8243ead4112eb6c66a57b8b9635357c28adbb9a8
Toraja <Toraja@users.noreply.github.com>
2023-03-10 18:48:06 +0900

    vim: move helm specific setting to helm ftplugin and reenable yamllint autocmd

--- vim/rtp/ftplugin/helm.lua
+++ vim/rtp/ftplugin/helm.lua
@@ -1,21 +1,31 @@
-vim.cmd('runtime! ftplugin/yaml.lua')
-
--- local surround_config = require('nvim-surround.config')
--- require('nvim-surround').buffer_setup({
---   surrounds = {
---     -- The below works but not useful
---     V = {
---       ---@diagnostic disable-next-line: assign-type-mismatch
---       add = { "{{ $", " }}" },
---       find = function()
---         return surround_config.get_selection({ pattern = '{{ ?%$[^}]* ?}}' })
---       end,
---       ---@diagnostic disable-next-line: assign-type-mismatch
---       delete = '^(....)().-( ?..)()$',
---       change = {
---         ---@diagnostic disable-next-line: assign-type-mismatch
---         target = '({{ %$)()[^}]*( }})()',
---       },
---     },
---   },
--- })
+require('nvim-surround').buffer_setup({
+  surrounds = {
+    v = {
+      ---@diagnostic disable-next-line: assign-type-mismatch
+      add = { "{{ ", " }}" },
+      find = function()
+        return require('nvim-surround.config').get_selection({ pattern = '{{ [^}]* }}' })
+      end,
+      ---@diagnostic disable-next-line: assign-type-mismatch
+      delete = '^(.. ?)().-( ?..)()$',
+      change = {
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        target = '({{ ?)()[^}]*( }})()',
+      },
+    },
+    -- -- The below works but not useful
+    -- V = {
+    --   ---@diagnostic disable-next-line: assign-type-mismatch
+    --   add = { "{{ $", " }}" },
+    --   find = function()
+    --     return surround_config.get_selection({ pattern = '{{ ?%$[^}]* ?}}' })
+    --   end,
+    --   ---@diagnostic disable-next-line: assign-type-mismatch
+    --   delete = '^(....)().-( ?..)()$',
+    --   change = {
+    --     ---@diagnostic disable-next-line: assign-type-mismatch
+    --     target = '({{ %$)()[^}]*( }})()',
+    --   },
+    -- },
+  }
+})
--- vim/rtp/ftplugin/yaml.lua
+++ vim/rtp/ftplugin/yaml.lua
@@ -1,21 +1,3 @@
-require('nvim-surround').buffer_setup({
-  surrounds = {
-    v = {
-      ---@diagnostic disable-next-line: assign-type-mismatch
-      add = { "{{ ", " }}" },
-      find = function()
-        return require('nvim-surround.config').get_selection({ pattern = '{{ [^}]* }}' })
-      end,
-      ---@diagnostic disable-next-line: assign-type-mismatch
-      delete = '^(.. ?)().-( ?..)()$',
-      change = {
-        ---@diagnostic disable-next-line: assign-type-mismatch
-        target = '({{ ?)()[^}]*( }})()',
-      },
-    },
-  }
-})
-
 function yaml_lint()
   vim.cmd("lgetexpr system('yamllint --format parsable ' . expand('%'))")
   if #(vim.fn.getloclist(0)) ~= 0 then
@@ -29,10 +11,11 @@ require('keymap.which-key-helper').register_for_ftplugin('Yaml', {
   l = { 'lua yaml_lint()', { desc = 'Lint', silent = true, buffer = true } },
 })
 
--- Disable autocmd as some yaml files are not genuine yaml file (such as helm chart).
--- local yaml_lint_augroud_id = vim.api.nvim_create_augroup('yaml_lint', {})
--- vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
---   group = yaml_lint_augroud_id,
---   pattern = '*.yaml',
---   callback = yaml_lint,
--- })
+local yaml_lint_augroud_id = vim.api.nvim_create_augroup('yaml_lint', {})
+vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+  group = yaml_lint_augroud_id,
+  buffer = 0,
+  callback = function()
+    yaml_lint()
+  end,
+})
