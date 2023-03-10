8370347d82fb52224a4ce9937dbbe410a295b2aa
Toraja <Toraja@users.noreply.github.com>
2023-03-10 23:29:37 +0900

    vim: add autocmd to format yaml

--- vim/rtp/ftplugin/yaml.lua
+++ vim/rtp/ftplugin/yaml.lua
@@ -7,10 +7,6 @@ function yaml_lint()
   end
 end
 
-require('keymap.which-key-helper').register_for_ftplugin('Yaml', {
-  l = { 'lua yaml_lint()', { desc = 'Lint', silent = true, buffer = true } },
-})
-
 local yaml_lint_augroud_id = vim.api.nvim_create_augroup('yaml_lint', {})
 vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
   group = yaml_lint_augroud_id,
@@ -18,4 +14,24 @@ vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
   callback = function()
     yaml_lint()
   end,
+})
+
+local function yaml_format()
+  preserve_cursor(function()
+    vim.cmd('silent %!yamlfmt -')
+  end)
+end
+
+local yaml_format_augroud_id = vim.api.nvim_create_augroup('yaml_format', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = yaml_format_augroud_id,
+  buffer = 0,
+  callback = function()
+    yaml_format()
+  end,
+})
+
+require('keymap.which-key-helper').register_for_ftplugin('Yaml', {
+  f = { 'lua yaml_format()', { desc = 'Format', silent = true, buffer = true } },
+  l = { 'lua yaml_lint()', { desc = 'Lint', silent = true, buffer = true } },
 })
