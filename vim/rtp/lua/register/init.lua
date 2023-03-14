2f4725690180b6f4871a0a88887a9c647a919f3f
Toraja <Toraja@users.noreply.github.com>
2023-03-15 05:11:46 +0900

    vim: add lua function to clear named registers

--- vim/rtp/lua/register/init.lua
+++ vim/rtp/lua/register/init.lua
@@ -19,3 +19,16 @@ vim.keymap.set('n', 'yp', function()
   vim.fn.setreg(clipboard_char(), current_file_path)
   print('Clipped:', current_file_path)
 end)
+
+function named_registers_clear()
+  local ascii_code_a, ascii_code_z = string.byte('a'), string.byte('z')
+  for i = ascii_code_a, ascii_code_z do
+    vim.fn.setreg(string.char(i), {})
+  end
+  -- Make the changes to registers permanent by running the command below,
+  -- or the registers will come back alive on the next launch of nvim
+  vim.cmd('wshada!')
+  print('All named registers have been cleared')
+end
+
+vim.api.nvim_create_user_command('NamedRegistersClear', named_registers_clear, {})
