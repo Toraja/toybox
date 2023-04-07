cf3df3c1fdf9869084f9ab44065dea7c8eafc1ec
Toraja <Toraja@users.noreply.github.com>
2023-04-07 14:35:54 +0900

    vim: open lazygit in float window

--- vim/rtp/lua/plugins.lua
+++ vim/rtp/lua/plugins.lua
@@ -1429,7 +1429,13 @@ return require('packer').startup(function(use)
         shell = 'fish',
       })
       vim.keymap.set('n', '<Leader>g',
-        function() require('toggleterm.terminal').Terminal:new({ cmd = 'lazygit', direction = 'tab' }):toggle() end,
+        function()
+          require('toggleterm.terminal').Terminal:new({
+            cmd = 'lazygit',
+            direction = 'float',
+            float_opts = { width = math.floor(vim.o.columns * 0.95), height = math.floor(vim.o.lines * 0.90) }
+          }):toggle()
+        end,
         { desc = 'lazygit' })
     end
   }
