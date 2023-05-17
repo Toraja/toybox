572f5350bd9be981ed8a0fdca5d74ae3ceec2e75
Toraja <Toraja@users.noreply.github.com>
2023-05-17 22:51:29 +0900

    vim: add greyjoy plugin

--- vim/rtp/lua/plugins/code-runner.lua
+++ vim/rtp/lua/plugins/code-runner.lua
@@ -26,4 +26,24 @@ return {
 			{ vim.g.chief_key .. "r", ":SnipRun<CR>", mode = { "v" }, desc = "sniprun" },
 		},
 	},
+	{
+		"desdic/greyjoy.nvim",
+		dependencies = {
+			{ "akinsho/toggleterm.nvim" },
+			{ "nvim-telescope/telescope.nvim" },
+		},
+		config = function()
+			local greyjoy = require("greyjoy")
+			greyjoy.setup({
+				output_results = "toggleterm",
+				last_first = true,
+				patterns = vim.list_extend(require("greyjoy.config").defaults.patterns, { "Makefile" }), -- patterns to find the root of the project
+			})
+			-- greyjoy.load_extension("generic")
+			-- greyjoy.load_extension("vscode_tasks")
+			greyjoy.load_extension("makefile")
+			-- greyjoy.load_extension("kitchen")
+			-- greyjoy.load_extension("cargo")
+		end,
+	},
 }
