caa613b2bf241eb53568d393f9ed8de7b2895868
Toraja <Toraja@users.noreply.github.com>
2023-08-30 23:09:42 +0900

    vim: add crates.nvim

--- vim/rtp/lua/plugins/filetype.lua
+++ vim/rtp/lua/plugins/filetype.lua
@@ -64,6 +64,24 @@ return {
 		"rust-lang/rust.vim",
 		ft = "rust",
 	},
+	{
+		"saecki/crates.nvim",
+		ft = "toml",
+		version = "*",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+		},
+		config = function()
+			require("crates").setup()
+			vim.api.nvim_create_autocmd("BufRead", {
+				group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+				pattern = "Cargo.toml",
+				callback = function()
+					require("cmp").setup.buffer({ sources = { { name = "crates" } } })
+				end,
+			})
+		end,
+	},
 	-- yaml
 	{
 		"someone-stole-my-name/yaml-companion.nvim",
