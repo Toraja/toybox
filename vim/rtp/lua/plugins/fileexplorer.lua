98393dc502fa98839092a50486d829b3a01426e1
Toraja <Toraja@users.noreply.github.com>
2023-10-05 21:57:20 +0900

    vim: bump neo-tree to v3

--- vim/rtp/lua/plugins/fileexplorer.lua
+++ vim/rtp/lua/plugins/fileexplorer.lua
@@ -92,14 +92,13 @@ return {
 	},
 	{
 		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
+		branch = "v3.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 			"MunifTanjim/nui.nvim",
 		},
 		config = function()
-			vim.g.neo_tree_remove_legacy_commands = 1
 			require("neo-tree").setup({
 				window = {
 					position = "float",
