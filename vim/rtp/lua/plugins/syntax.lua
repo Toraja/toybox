608a8fd88467f62954c850c32f962b8c6931b324
Toraja <Toraja@users.noreply.github.com>
2023-10-24 17:18:31 +0900

    vim: add nvim-lint plugin

--- vim/rtp/lua/plugins/syntax.lua
+++ vim/rtp/lua/plugins/syntax.lua
@@ -114,4 +114,22 @@ return {
 			})
 		end,
 	},
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {
+				go = { "golangcilint" },
+				yaml = { "yamllint" },
+			}
+
+			vim.api.nvim_create_autocmd("BufWritePost", {
+				group = vim.api.nvim_create_augroup("AutoLint", {}),
+				pattern = "*",
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
 }
