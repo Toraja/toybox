d999244777a3e5d3b6507d1afd6d66c5301d2a5b
Toraja <Toraja@users.noreply.github.com>
2023-12-06 18:03:41 +0900

    vim: apply formatting

--- vim/rtp/lua/qf/init.lua
+++ vim/rtp/lua/qf/init.lua
@@ -1,31 +1,35 @@
 local M = {}
 
 local function quickfix_list_toggle()
-  local qf_win_id = vim.fn.getqflist({ winid = 0 }).winid
-  if qf_win_id == 0 then
-    local current_win_id = vim.fn.win_getid()
-    if not pcall(function() vim.cmd('copen') end) then
-      print('No quickfix list')
-      return
-    end
-    vim.fn.win_gotoid(current_win_id)
-  else
-    vim.cmd('cclose')
-  end
+	local qf_win_id = vim.fn.getqflist({ winid = 0 }).winid
+	if qf_win_id == 0 then
+		local current_win_id = vim.fn.win_getid()
+		if not pcall(function()
+			vim.cmd("copen")
+		end) then
+			print("No quickfix list")
+			return
+		end
+		vim.fn.win_gotoid(current_win_id)
+	else
+		vim.cmd("cclose")
+	end
 end
 
 local function location_list_toggle()
-  local loclist_win_id = vim.fn.getloclist(0, { winid = 0 }).winid
-  if loclist_win_id == 0 then
-    local current_win_id = vim.fn.win_getid()
-    if not pcall(function() vim.cmd('lopen') end) then
-      print('No location list')
-      return
-    end
-    vim.fn.win_gotoid(current_win_id)
-  else
-    vim.cmd('lclose')
-  end
+	local loclist_win_id = vim.fn.getloclist(0, { winid = 0 }).winid
+	if loclist_win_id == 0 then
+		local current_win_id = vim.fn.win_getid()
+		if not pcall(function()
+			vim.cmd("lopen")
+		end) then
+			print("No location list")
+			return
+		end
+		vim.fn.win_gotoid(current_win_id)
+	else
+		vim.cmd("lclose")
+	end
 end
 
 local function qf_files_open()
@@ -56,11 +60,11 @@ local function qf_files_open()
 end
 
 function M.setup(opts)
-  opts = opts or {}
-  vim.keymap.set('n', '<C-q><C-q>', quickfix_list_toggle, { desc = 'Toggle quickfix window' })
-  vim.keymap.set('n', '<C-q><C-l>', location_list_toggle, { desc = 'Toggle loation list window' })
+	opts = opts or {}
+	vim.keymap.set("n", "<C-q><C-q>", quickfix_list_toggle, { desc = "Toggle quickfix window" })
+	vim.keymap.set("n", "<C-q><C-l>", location_list_toggle, { desc = "Toggle loation list window" })
 
-  vim.api.nvim_create_user_command('QfFilesOpen', qf_files_open, {})
+	vim.api.nvim_create_user_command("QfFilesOpen", qf_files_open, {})
 end
 
 return M
