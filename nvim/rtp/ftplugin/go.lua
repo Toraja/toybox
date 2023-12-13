4cfe7bad3332b406ee160f5674efc5dc7d0e3f4e
Toraja <Toraja@users.noreply.github.com>
2023-12-13 16:13:29 +0900

    nvim: add keymap to print ginkgo command

--- nvim/rtp/ftplugin/go.lua
+++ nvim/rtp/ftplugin/go.lua
@@ -12,18 +12,21 @@ require("keymap.which-key-helper").register_for_ftplugin({
 	T = { "GoAddTag", { desc = "Add tag to struct" } },
 	v = { "GoCoverage", { desc = "Show test coverage" } },
 	V = { "GoCoverage -t", { desc = "Load coverage file" } },
+	["<Space>g"] = { "lua ginkgo_generate()", { desc = "Generate gingko test file" } },
+	["<Space>p"] = { "lua print(ginkgo_get_nearest_spec_cmd())", { desc = "Print nearest ginkgo spec command" } },
+	["<Space>P"] = { "lua print(ginkgo_get_this_file_cmd())", { desc = "Print ginkgo specs on this file command" } },
 	["<Space>t"] = { "lua ginkgo_test_nearest_spec()", { desc = "Test nearest ginkgo spec" } },
 	["<Space>T"] = { "lua ginkgo_test_this_file()", { desc = "Test ginkgo specs on this file" } },
-	["<Space>g"] = { "lua ginkgo_generate()", { desc = "Generate gingko test file" } },
 })
 
-local function run_ginkgo_cmd_in_terminal(ginkgo_cmd)
-	-- Without sleep, echoed messsge is sometimes  not displayed
+local function ginkgo_run_cmd_in_terminal(ginkgo_cmd)
+	-- Without sleep, echoed messsge is sometimes not displayed
 	local cmd = "sleep 0.1 && echo '@@@ " .. ginkgo_cmd .. " @@@' && " .. ginkgo_cmd
 	vim.cmd("tabnew | terminal " .. cmd)
 end
 
-function ginkgo_test_nearest_spec()
+---@return string
+function ginkgo_get_nearest_spec_cmd()
 	local ginkgo_spec_patterns = {
 		'Describe%("(.*)",.*func%(%)%s{',
 		'Context%("(.*)",.*func%(%)%s{',
@@ -46,24 +49,33 @@ function ginkgo_test_nearest_spec()
 	end
 	local found_spec = find_spec()
 	if found_spec == nil then
-		print("No spec was found")
-		return
+		return ""
 	end
 
 	local test_file_path = vim.api.nvim_buf_get_name(0)
 	local test_file_basename = vim.fs.basename(test_file_path)
 	local test_file_dir = vim.fs.dirname(test_file_path)
-	local ginkgo_cmd =
-		string.format('ginkgo --focus-file "%s" --focus "%s" %s', test_file_basename, found_spec, test_file_dir)
-	run_ginkgo_cmd_in_terminal(ginkgo_cmd)
+
+	return string.format('ginkgo --focus-file "%s" --focus "%s" %s', test_file_basename, found_spec, test_file_dir)
 end
 
-function ginkgo_test_this_file()
+function ginkgo_test_nearest_spec()
+	local ginkgo_cmd = ginkgo_get_nearest_spec_cmd()
+	if ginkgo_cmd == "" then
+		print("No spec was found")
+	end
+	ginkgo_run_cmd_in_terminal(ginkgo_cmd)
+end
+
+function ginkgo_get_this_file_cmd()
 	local test_file_path = vim.api.nvim_buf_get_name(0)
 	local test_file_basename = vim.fs.basename(test_file_path)
 	local test_file_dir = vim.fs.dirname(test_file_path)
-	local ginkgo_cmd = string.format('ginkgo --focus-file "%s" %s', test_file_basename, test_file_dir)
-	run_ginkgo_cmd_in_terminal(ginkgo_cmd)
+	return string.format('ginkgo --focus-file "%s" %s', test_file_basename, test_file_dir)
+end
+
+function ginkgo_test_this_file()
+	ginkgo_run_cmd_in_terminal(ginkgo_get_this_file_cmd())
 end
 
 function ginkgo_generate()
