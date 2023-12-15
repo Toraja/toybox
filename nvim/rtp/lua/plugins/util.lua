f117f7ac54e18ffa938b6b9f72bf113d55261146
Toraja <Toraja@users.noreply.github.com>
2023-12-15 23:29:43 +0900

    nvim: fix so that whole notification message is visible

--- nvim/rtp/lua/plugins/util.lua
+++ nvim/rtp/lua/plugins/util.lua
@@ -7,13 +7,27 @@ return {
 		},
 		config = function()
 			vim.notify = require("notify")
+
+			local max_width = vim.opt.columns:get() - 6 -- width does not include border
 			require("notify").setup({
 				background_colour = "#000000",
+				max_width = max_width,
+				on_open = function(win_id, notif)
+					vim.api.nvim_win_set_option(win_id, "wrap", true)
+
+					local win_height = 2 -- for notification title and separator
+					-- each of notif.message is output to separate line
+					for _, msg in ipairs(notif.message) do
+						win_height = win_height + math.ceil(string.len(msg) / max_width)
+					end
+					vim.api.nvim_win_set_height(win_id, win_height)
+				end,
 				stages = "fade",
 			})
 			local keymap_amend = require("keymap-amend")
 			keymap_amend("n", "<M-c>", function(original)
 				original()
+				---@diagnostic disable-next-line: undefined-field
 				vim.notify.dismiss()
 			end, { desc = "Close notifications" })
 		end,
