18e511b32c4281be66946e19d4048cf43c28bacd
Toraja <Toraja@users.noreply.github.com>
2024-01-16 15:05:58 +0900

    nvim: modify register_with_editable to accept function

--- nvim/rtp/lua/keymap/which-key-helper.lua
+++ nvim/rtp/lua/keymap/which-key-helper.lua
@@ -1,35 +1,43 @@
 local M = {}
 
 local function wrap_as_cmd(cmd)
-  return '<Cmd>' .. cmd .. '<CR>'
+	return "<Cmd>" .. cmd .. "<CR>"
 end
 
 local function wrap_as_editable(cmd)
-  return ':' .. cmd .. ' '
+	return ":" .. cmd .. " "
 end
 
 function M.register_with_editable(prompt, prefix_key, edit_key, cmds, opts)
-  local edit_prefix_key = prefix_key .. edit_key
-  local wk = require("which-key")
-  -- Use which-key only to set the name of grouping
-  wk.register({ [prefix_key] = { name = prompt } }, opts)
-  wk.register({ [edit_prefix_key] = { name = "Edit command" } }, opts)
+	local edit_prefix_key = prefix_key .. edit_key
+	local wk = require("which-key")
+	-- Use which-key only to set the name of grouping
+	wk.register({ [prefix_key] = { name = prompt } }, opts)
+	wk.register({ [edit_prefix_key] = { name = "Edit command" } }, opts)
 
-  for key, cmd in pairs(cmds) do
-    vim.keymap.set('n', prefix_key .. key, wrap_as_cmd(cmd[1]), cmd[2])
-    vim.keymap.set('n', edit_prefix_key .. key, wrap_as_editable(cmd[1]), cmd[2])
-  end
+	for key, cmd in pairs(cmds) do
+		if type(cmd[1]) == "function" then
+			vim.keymap.set("n", prefix_key .. key, cmd[1], cmd[2])
+			goto continue
+		end
+
+		vim.keymap.set("n", prefix_key .. key, wrap_as_cmd(cmd[1]), cmd[2])
+		vim.keymap.set("n", edit_prefix_key .. key, wrap_as_editable(cmd[1]), cmd[2])
+
+		::continue::
+	end
 end
 
 local function make_cmds_buffer_local(cmds)
-  for _, cmd in pairs(cmds) do
-    cmd[2].buffer = true
-  end
+	for _, cmd in pairs(cmds) do
+		cmd[2].buffer = true
+	end
 end
 
 function M.register_for_ftplugin(cmds)
-  make_cmds_buffer_local(cmds)
-  M.register_with_editable(vim.opt.filetype:get(), '<LocalLeader>', '<LocalLeader>', cmds, { buffer = 0 })
+	make_cmds_buffer_local(cmds)
+	---@diagnostic disable-next-line: undefined-field
+	M.register_with_editable(vim.opt.filetype:get(), "<LocalLeader>", "<LocalLeader>", cmds, { buffer = 0 })
 end
 
 return M
