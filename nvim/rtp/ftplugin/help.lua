1b407e2c04bfcb622a3e13fea9df396e190f210b
Toraja <Toraja@users.noreply.github.com>
2024-07-26 11:21:43 +0900

    nvim: fix keymap for help

--- nvim/rtp/ftplugin/help.lua
+++ nvim/rtp/ftplugin/help.lua
@@ -1,7 +1,4 @@
-local wk = require("which-key")
-wk.register({
-	["<C-]>"] = { "<C-]>", "Help" },
-	["<C-w><C-]>"] = { "<C-w>g<C-]>", "Help [horz]" },
-	[vim.g.vert_key .. "<C-]>"] = { "<C-w><C-v>g<C-]>", "Help [vert]" },
-	["<C-t><C-]>"] = { "<C-w><C-v>g<C-]><C-w>T", "Help [tab]" },
-}, { buffer = 0 })
+vim.keymap.set("n", "<C-]>", "<C-]>", { buffer = true, desc = "Help" }) -- <C-]> is mapped to LSP stuff so remap to default
+vim.keymap.set("n", "<C-w><C-]>", "<C-w>g<C-]>", { buffer = true, desc = "Help [horz]" })
+vim.keymap.set("n", vim.g.vert_key .. "<C-]>", "<C-w><C-v>g<C-]>", { buffer = true, desc = "Help [vert]" })
+vim.keymap.set("n", "<C-t><C-]>", "<C-w><C-v>g<C-]><C-w>T", { buffer = true, desc = "Help [tab]" })
