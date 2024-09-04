668c514085b9743e9b0d28f54f93ef371d1b1a10
Toraja <Toraja@users.noreply.github.com>
2024-09-04 21:23:42 +0900

    nvim: change lua_ls setup to use on_init as written on the doc
    https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls

--- nvim/rtp/lua/plugins/lsp.lua
+++ nvim/rtp/lua/plugins/lsp.lua
@@ -39,25 +39,19 @@ return {
 
 			local servers = {
 				lua_ls = {
-					settings = {
-						Lua = {
+					on_init = function(client)
+						local path = client.workspace_folders[1].name
+						if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
+							return
+						end
+
+						client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
 							runtime = {
-								-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+								-- Tell the language server which version of Lua you're using
+								-- (most likely LuaJIT in the case of Neovim)
 								version = "LuaJIT",
 							},
-							diagnostics = {
-								-- Get the language server to recognize the `vim` global
-								globals = { "vim" },
-							},
-							workspace = {
-								-- Make the server aware of Neovim runtime files
-								library = vim.api.nvim_get_runtime_file("", true),
-								checkThirdParty = false,
-							},
-							-- Do not send telemetry data containing a randomized but unique identifier
-							telemetry = {
-								enable = false,
-							},
+							-- Make the server aware of Neovim runtime files
 							format = {
 								enable = true,
 								-- Put format options here
@@ -66,6 +60,30 @@ return {
 									indent_style = "space",
 									indent_size = "2",
 								},
+							},
+							telemetry = {
+								enable = false,
+							},
+							workspace = {
+								checkThirdParty = false,
+								-- library = {
+								-- 	vim.env.VIMRUNTIME,
+								-- Depending on the usage, you might want to add additional paths here.
+								-- "${3rd}/luv/library"
+								-- "${3rd}/busted/library",
+								-- },
+								-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+								library = vim.api.nvim_get_runtime_file("", true),
+							},
+							-- Do not send telemetry data containing a randomized but unique identifier
+						})
+					end,
+					settings = {
+						Lua = {
+							diagnostics = {
+								-- Get the language server to recognize the `vim` global
+								-- Somehow it does not work if this setting is in `on_init`
+								globals = { "vim" },
 							},
 						},
 					},
