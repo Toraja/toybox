3c2490391390470af5b4d544a9b69f6ac270dd9a
Toraja <Toraja@users.noreply.github.com>
2024-09-11 17:01:04 +0900

    nvim: add setup for json lsp

--- nvim/rtp/ftplugin/json.lua
+++ nvim/rtp/ftplugin/json.lua
@@ -1,3 +1,9 @@
 vim.bo.expandtab = true
 vim.bo.shiftwidth = 2
 vim.bo.tabstop = 2
+
+if vim.fn.executable("vscode-json-language-server") == 1 then
+	require("lspconfig")["jsonls"].setup({
+		capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	})
+end
