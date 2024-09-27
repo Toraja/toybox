73bcd62498c0f04794fe3f3969f1618f14b1cc42
Toraja <Toraja@users.noreply.github.com>
2024-09-27 12:04:16 +0900

    taskwarrior: modify tui keymap and fix list-shortcuts.sh not to expand glob

--- taskwarrior/shortcut/list-shortcuts.sh
+++ taskwarrior/shortcut/list-shortcuts.sh
@@ -9,20 +9,23 @@ num_commands="$(echo "$shortcuts" | grep shortcuts | sed -e 's/shortcuts\.//')"
 declare -A num_command_map
 while read line; do
   arr=($line)
-  num=${arr[0]} 
+  num=${arr[0]}
   cmd=$(basename ${arr[1]})
-  rest=${arr[2]} 
+  rest=${arr[2]}
   num_command_map+=([$num]="$cmd $rest")
 done <<< "$num_commands"
 
+shell_opts=$(set +o)
+set -f # disable globbing (i.e. * being expanded)
 num_keys=$(echo "$shortcuts" | grep keyconfig | sed -e 's/keyconfig\.shortcut//')
 declare -A num_key_map
 while read line; do
   arr=($line)
-  num=${arr[0]} 
-  key=${arr[1]} 
+  num=${arr[0]}
+  key=${arr[1]}
   num_key_map+=([$num]="$key")
 done <<< "$num_keys"
+eval "$shell_opts"
 
 for num in "${!num_command_map[@]}"
 do
