8923674c3b228d082849ef39c40513338b8305ce
Toraja <Toraja@users.noreply.github.com>
2024-09-30 21:48:27 +0900

    nvim: fix git#is_inside_work_tree()

--- nvim/rtp/lua/git/init.lua
+++ nvim/rtp/lua/git/init.lua
@@ -7,12 +7,13 @@ function M.root_path()
 	return string.gsub(vim.trim(root_path), "^" .. vim.uv.os_homedir(), "~", 1)
 end
 
+---@return boolean
 function M.is_inside_work_tree()
-	local output = assert(io.popen("git rev-parse --is-inside-work-tree", "r"))
-	local is_inside_work_tree = assert(output:read("*a"))
+	local output = assert(io.popen("git rev-parse --is-inside-work-tree 2>/dev/null", "r"))
+	local is_inside_work_tree = vim.trim(assert(output:read("*a")))
 	output:close()
 
-	return is_inside_work_tree == true
+	return is_inside_work_tree == "true"
 end
 
 return M
