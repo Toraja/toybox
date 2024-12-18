28ced6547e2513623182307f217b872f92c89e47
Toraja <Toraja@users.noreply.github.com>
2024-12-18 11:29:55 +0900

    yazi add plugins

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,1 +1,20 @@
--- plugin page (https://yazi-rs.github.io/docs/plugins/overview/) says this file is necessary but what should I put here?
+require("full-border"):setup({
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+})
+
+require("git"):setup()
+
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line({
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	})
+end, 500, Status.RIGHT)
