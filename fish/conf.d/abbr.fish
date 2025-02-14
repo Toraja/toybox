5bfcaed91eb350ca32f54f2440a56310db69b963
Toraja <Toraja@users.noreply.github.com>
2025-02-14 20:43:34 +0900

    fish: use long flag in abbrev of glab mr create

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -120,8 +120,8 @@ abbr --add --global glin glab issue note
 abbr --add --global gliu glab issue update
 abbr --add --global gliv glab issue view
 abbr --add --global glm glab mr
-abbr --add --global glmc glab mr create --remove-source-branch -s \(git branch --show-current\) --fill
-abbr --add --global glmcl glab mr create --remove-source-branch --copy-issue-labels -s \(git branch --show-current\) --fill
+abbr --add --global glmc glab mr create --remove-source-branch --source-branch \(git branch --show-current\) --fill
+abbr --add --global glmcl glab mr create --remove-source-branch --source-branch \(git branch --show-current\) --fill --copy-issue-labels --related-issue
 abbr --add --global glmd glab mr diff
 abbr --add --global glml glab mr list
 abbr --add --global glmlr glab mr list --reviewer=@me
