b3efba385e50a8861eba701ef366b1c45a94a9f3
Toraja <Toraja@users.noreply.github.com>
2025-02-28 12:53:55 +0900

    fish: add abbrev for glab ci view

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -111,6 +111,7 @@ abbr --add --global gl glab
 abbr --add --global glc glab ci
 abbr --add --global glcl glab ci list
 abbr --add --global glcs glab ci status --live
+abbr --add --global glcv glab ci view
 abbr --add --global gli glab issue
 abbr --add --global glic glab issue create --title
 abbr --add --global glil glab issue list
