0e597e3cd780c948184de73fe03935c0265ee955
Toraja <Toraja@users.noreply.github.com>
2025-04-04 22:48:12 +0900

    fish: add/modify abbrev

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -57,11 +57,12 @@ abbr --add --global gfo git fetch origin \(git remote show origin \| grep 'HEAD 
 abbr --add --global gfp git fetch --prune --prune-tags
 abbr --add --global gftf git fetch --tags --force
 abbr --add --global gg git log
-abbr --add --global ggu git log origin..HEAD
+abbr --add --global ggc git log \(git branch --show-current\)..main
 abbr --add --global ggc git log --oneline \| fzf \| awk '\'{print $1}\''
 abbr --add --global ggs git log --oneline --name-status
+abbr --add --global ggu git log origin..HEAD
 abbr --add --global gps git push
-abbr --add --global gpl git pull
+abbr --add --global gpl git pull --rebase
 abbr --add --global gpld git pull-default
 abbr --add --global gr git reset
 abbr --add --global groh git reset-origin --hard
