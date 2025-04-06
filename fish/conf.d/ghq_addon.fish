4733a48c4f386aac48bb7723f3858d9a44bda2e5
Toraja <Toraja@users.noreply.github.com>
2025-04-06 22:19:20 +0900

    fish: add comment

--- fish/conf.d/ghq_addon.fish
+++ fish/conf.d/ghq_addon.fish
@@ -1,2 +1,3 @@
+# This requires fish-ghq plugin
 complete -f -c ghq -n __fish_ghq_needs_command -a dirty -d "List path of dirty repositories"
 complete -f -c ghq -n "__fish_ghq_using_command dirty" -l status -s s -d "Display the status of each repository"
