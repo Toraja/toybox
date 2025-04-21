ef1feb65a07a9ec7d9e96de7f9f939f7763f6396
Toraja <Toraja@users.noreply.github.com>
2025-04-21 14:57:39 +0900

    fish: improve reload_keybind.

--- fish/functions/reload_keybind.fish
+++ fish/functions/reload_keybind.fish
@@ -1,8 +1,10 @@
 function reload_keybind
-    commandline --replace ''
+    # Clear commandline first to prevent a command from being executed if commandline buffer contains something
+    commandline --function clear-commandline
     fish_user_key_bindings
-    # add trailing spaces so that messages will not cut off when buffer contains something
-    and echo -n 'reloaded keybind               '
-    or echo -n 'failed to reload keybind               '
+    # XXX: When commandline buffer contains something, that hides the left side of the below messages.
+    # Appending (not prepending) spaces somehow unhides the messages.
+    and printf '%s %s' '[ reloaded keybind ]' $(string repeat --count 30 ' ')
+    or printf '%s %s' '[ failed to reloaded keybind ]' $(string repeat --count 30 ' ')
     commandline --function execute
 end
