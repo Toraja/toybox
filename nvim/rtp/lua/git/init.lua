a873cd15feed38c86cbca42722f20fc7cd0b5516
Toraja <Toraja@users.noreply.github.com>
2025-06-25 16:48:11 +0900

    nvim: function to get git root of buffer

--- nvim/rtp/lua/git/init.lua
+++ nvim/rtp/lua/git/init.lua
@@ -1,11 +1,20 @@
 local M = {}
 
---- Return the root path of given git repository, or nil if the given path is not git repository.
+--- Return the git root path of given path, or nil if the given path is not in git repository.
 ---@param path string? Current directory is used if not specified.
 ---@return string?
 function M.root_path(path)
 	path = path or vim.uv.cwd()
 	return vim.fs.root(path, { ".git" })
+end
+
+--- Return the git root path of given buffer, or nil if the given buffer is not in git repository.
+---@param buffer integer? Current buffer is used if not specified.
+---@return string?
+function M.buf_root_path(buffer)
+	buffer = buffer or vim.api.nvim_get_current_buf()
+	local path = vim.api.nvim_buf_get_name(buffer)
+	return M.root_path(path)
 end
 
 --- Return the path to the given path relative to git root, or nil if the given path is not in git repository.
