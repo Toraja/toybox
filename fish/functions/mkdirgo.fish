125f58e2e14947308d681067eecf3a59f367e537
Toraja <Toraja@users.noreply.github.com>
2025-07-18 21:34:24 +0900

    fish: apply formatter

--- fish/functions/mkdirgo.fish
+++ fish/functions/mkdirgo.fish
@@ -1,12 +1,12 @@
 function mkdirgo --wraps=mkdir --description 'Create a directory and set CWD'
-	command mkdir $argv
-	if test $status = 0
-		switch $argv[(count $argv)]
-			case '-*'
-				# last arg was option, do nothing
-			case '*'
-				cd $argv[(count $argv)]
-			return
-		end
-	end
+    command mkdir $argv
+    if test $status = 0
+        switch $argv[(count $argv)]
+            case '-*'
+                # last arg was option, do nothing
+            case '*'
+                cd $argv[(count $argv)]
+                return
+        end
+    end
 end
