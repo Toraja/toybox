b8de980daf74877784bbaaa2168313c595ff7d26
Toraja <Toraja@users.noreply.github.com>
2025-10-28 13:54:57 +0900

    nvim: remove jsonls setup in json.lua as it is already setup in lsp.lua

--- nvim/rtp/ftplugin/json.lua
+++ nvim/rtp/ftplugin/json.lua
@@ -1,9 +1,3 @@
 vim.bo.expandtab = true
 vim.bo.shiftwidth = 2
 vim.bo.tabstop = 2
-
-if vim.fn.executable("vscode-json-language-server") == 1 then
-	vim.lsp.config("jsonls", {
-		capabilities = require("cmp_nvim_lsp").default_capabilities(),
-	})
-end
