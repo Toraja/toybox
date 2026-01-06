8dbd3f09ee6bcedc2d09c246193da548eeaa76da
Toraja <Toraja@users.noreply.github.com>
2026-01-06 22:07:54 +0900

    add -w option to tmux set/load-buffer to send the buffer contents to system clipboard if possible

--- fish/functions/clip_to_tmux.fish
+++ fish/functions/clip_to_tmux.fish
@@ -1,10 +1,4 @@
 function clip_to_tmux --description 'send stdin to tmux buffer and clipboard (if any command is available)'
     read stdin
-    echo -n $stdin | tmux load-buffer -
-    # $clipbin is set inside fish_user_key_bindings()
-    # Somehow empty pipe is fine if it is a result of `eval`
-    # (stdin will be discarded)
-    # Mere variable expansion (without `eval`) results in error
-    echo -n $stdin | eval $clipbin
+    echo -n $stdin | tmux load-buffer -w -
 end
-
