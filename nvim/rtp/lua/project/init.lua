cad8a3b25190a6ed8e12d8b34fa6828621458314
Toraja <Toraja@users.noreply.github.com>
2026-01-09 11:09:21 +0900

    nvim: simplify auto_pj_root auto command

--- nvim/rtp/lua/project/init.lua
+++ nvim/rtp/lua/project/init.lua
@@ -25,29 +25,16 @@ function M.get_root_path(path)
 	return vim.fs.root(path, pj_root_markers) or cwd
 end
 
---- lcd to the current project root of the specified buffer.
---- Nothing happens if none of the marker matches or if current buffer has no name.
----@param buf_id integer
-function M.lcd_to_root_path(buf_id)
-	local buf_path = vim.api.nvim_buf_get_name(buf_id)
-	if buf_path == "" then
-		return
-	end
-
-	local root_path = M.get_root_path(buf_path)
-	if not root_path then
-		return
-	end
-
-	vim.cmd("lcd " .. root_path)
-end
-
 function M.setup()
 	local auto_pj_root_augroup_id = vim.api.nvim_create_augroup("auto_pj_root", {})
 	vim.api.nvim_create_autocmd("BufEnter", {
 		group = auto_pj_root_augroup_id,
 		callback = function(params)
-			M.lcd_to_root_path(params.buf)
+			local root = vim.fs.root(params.buf, pj_root_markers)
+			if not root then
+				return
+			end
+			vim.cmd("lcd " .. root)
 		end,
 	})
 end
