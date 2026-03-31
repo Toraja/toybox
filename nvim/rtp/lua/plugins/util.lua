6d2b33ab67265663b095ffc2a1332430f2a54747
Toraja <Toraja@users.noreply.github.com>
2026-03-31 22:34:15 +0900

    nvim: disable luarocks

--- nvim/rtp/lua/plugins/util.lua
+++ nvim/rtp/lua/plugins/util.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"vhyrro/luarocks.nvim",
+		enabled = false,
 		priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
 		config = true,
 	},
