38860e914337229be67f5af657efc2851cdf8897
Toraja <Toraja@users.noreply.github.com>
2026-06-09 18:18:13 +0900

    nvim: remove nvim-cmp setup in crates.nvim as nvim-cmp is no longer used

--- nvim/rtp/lua/plugins/filetype.lua
+++ nvim/rtp/lua/plugins/filetype.lua
@@ -179,13 +179,6 @@ return {
 		},
 		config = function()
 			require("crates").setup()
-			vim.api.nvim_create_autocmd("BufRead", {
-				group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
-				pattern = "Cargo.toml",
-				callback = function()
-					require("cmp").setup.buffer({ sources = { { name = "crates" } } })
-				end,
-			})
 		end,
 	},
 	-- markup languages
