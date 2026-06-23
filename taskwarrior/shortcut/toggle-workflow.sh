1eeba8dbcc432cad6c26d6e941f1d41bae490058
Toraja <Toraja@users.noreply.github.com>
2026-06-23 16:15:54 +0900

    taskwarrior: stop task when workflow is set to onhold

--- taskwarrior/shortcut/toggle-workflow.sh
+++ taskwarrior/shortcut/toggle-workflow.sh
@@ -12,4 +12,7 @@ if [ "$(task $task_id _unique workflow)" == $workflow_name ]; then
   task $task_id modify 'workflow:' > /dev/null
 else
   task $task_id modify "workflow:$workflow_name" > /dev/null
+  if [ $workflow_name == "onhold" ]; then
+    task stop $task_id > /dev/null || true
+  fi
 fi
